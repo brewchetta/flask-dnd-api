f246131ce8f8389bb3a772904b7d6cc43f8c1774
brewchetta <chett.tiller@gmail.com>
2024-02-27 09:49:07 -0500

    added query params for name, size, and categories

--- server/app.py
+++ server/app.py
@@ -27,13 +27,36 @@ def index():
 def get_monsters():
     PAGE = request.args.get('page') or 1
     PER_PAGE = request.args.get('per_page') or 10
+    NAME_QUERY = request.args.get('name')
+    CATEGORY_QUERY = request.args.get('category')
+    SUBCATEGORY_QUERY = request.args.get('sub_category')
+    SIZE_QUERY = request.args.get('size')
     OFFSET = (PAGE - 1) * PER_PAGE
 
-    monsters = Monster.query.limit(PER_PAGE).offset(OFFSET).all()
-    return [ m.to_dict() for m in monsters ]
+    if NAME_QUERY or CATEGORY_QUERY or SUBCATEGORY_QUERY or SIZE_QUERY:
+        monsters = Monster.query.where(
+            Monster.name.like(f"%{NAME_QUERY}%") | 
+            Monster.category.like(f"%{CATEGORY_QUERY}%") | 
+            Monster.sub_category.like(f"%{SUBCATEGORY_QUERY}%") | 
+            Monster.size.like(f"%{SIZE_QUERY}%")
+        ).limit(PER_PAGE).offset(OFFSET).all()
+    else:
+        monsters = Monster.query.limit(PER_PAGE).offset(OFFSET).all()
+
+    return [ m.to_dict() for m in monsters ], 200
+
+
+# GET MONSTER #########
+# return Monster:dict
+#######################
+@app.get('/monsters/<int:id>')
+def get_monster_by_id(id):
+    monster = Monster.query.where(Monster.id == id).first()
+    return monster.to_dict(), 200
 
 
 # POST MONSTER #########
+# return Monster:dict
 #######################
 @app.post('/monsters')
 def post_monster():
@@ -49,4 +72,4 @@ def post_monster():
 
 
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5000, debug=True)
